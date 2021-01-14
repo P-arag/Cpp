598883e629acd83859333e6646a895860969878a
Parag Jyoti Pal <pal.parag257@gmail.com>
2021-01-14 21:03:44 +0530

    Initial Commit

--- printing.cpp
+++ printing.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
+    string out = "The addition is:- ";
     int num1, num2;
     cout << "Enter the first number:- ";
     cin >> num1;
     cout << "Enter the second number:- ";
     cin >> num2;
-    cout << "The addition is:- " << num1 + num2;
+    cout << out << num1 + num2;
+    getche();
     return 0;
 }
