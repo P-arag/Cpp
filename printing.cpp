aa05f69a460fddda7e3f6efffd9fe4a3e9c502b1
Parag Jyoti Pal <pal.parag257@gmail.com>
2021-01-14 17:35:35 +0530

    Initial Commit

---
+++ printing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int inp;
+    cout << "Hello World" << endl;
+    cin >> inp;
+    cout << "Your integer:- " << inp;
+    return 0;
+}
